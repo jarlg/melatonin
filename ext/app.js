91e2bca549f06eea62408a08b63811e7dece2db7
Jarl Gunnar T. Flaten <jarl.flaten@gmail.com>
2014-05-30 09:26:45 +0200

    remove css/js toggle - verified that js works whenever, and is quicker

--- ext/app.js
+++ ext/app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var css_radio, js_radio, opacity_input, port, toggle_css_msg;
+var opacity_input, port;
 
 opacity_input = document.getElementById('opacity');
 
@@ -14,35 +14,6 @@ opacity_input.addEventListener('input', function(event) {
     type: 'update_current_opacity',
     opacity: opacity_input.value
   });
-});
-
-js_radio = document.getElementById('js-radio');
-
-css_radio = document.getElementById('css-radio');
-
-chrome.runtime.sendMessage({
-  type: 'get_css_opt'
-}, function(response) {
-  if (response.css) {
-    return css_radio.checked = true;
-  } else {
-    return js_radio.checked = true;
-  }
-});
-
-toggle_css_msg = function(val) {
-  return chrome.runtime.sendMessage({
-    type: 'update_css_opt',
-    css: val
-  });
-};
-
-js_radio.addEventListener('click', function() {
-  return toggle_css_msg(false);
-});
-
-css_radio.addEventListener('click', function() {
-  return toggle_css_msg(true);
 });
 
 port = chrome.runtime.connect({
