897cc4adfaf828168de5bc05396a1c19e3fce071
Jarl Gunnar T. Flaten <jarl.flaten@gmail.com>
2014-07-31 12:30:52 +0200

    add build

--- ext/app.js
+++ ext/app.js
@@ -10,33 +10,33 @@ custom_toggle = document.getElementById('custom-toggle');
 colorpicker = document.getElementById('colorpicker');
 
 chrome.storage.local.get(['on', 'opacity', 'custom', 'custom_color'], function(items) {
-  opacity_input.value = items.opacity;
   on_off_toggle.checked = items.on;
   custom_toggle.checked = items.custom;
-  return colorpicker = items.custom_color;
+  opacity_input.value = items.opacity;
+  return colorpicker.value = items.custom_color;
 });
 
 opacity_input.addEventListener('input', function(event) {
   return chrome.storage.local.set({
-    'opacity': opacity_input.value
+    'opacity': this.value
+  }, function() {});
+});
+
+colorpicker.addEventListener('input', function(event) {
+  return chrome.storage.local.set({
+    'custom_color': this.value
   }, function() {});
 });
 
 on_off_toggle.addEventListener('click', function(event) {
   return chrome.storage.local.set({
-    'on': on_off_toggle.checked
+    'on': this.checked
   }, function() {});
 });
 
 custom_toggle.addEventListener('click', function(event) {
   return chrome.storage.local.set({
-    'on': custom_toggle.checked
-  }, function() {});
-});
-
-colorpicker.addEventListener('click', function(event) {
-  return chrome.storage.local.set({
-    'on': colorpicker.value
+    'custom': this.checked
   }, function() {});
 });
 
--- ext/content_script.js
+++ ext/content_script.js
@@ -1,5 +1,39 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var init_overlay, set_bgcolor, update_color;
+var obj;
+
+obj = {
+  rgb_to_hex: function(rgb) {
+    return "#" + ((1 << 24) + (rgb.r << 16) + (rgb.g << 8) + rgb.b).toString(16).slice(1);
+  },
+  hex_to_rgb: function(hex) {
+    var result, shorthandRegex;
+    shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+    result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    if (result) {
+      return {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      };
+    } else {
+      return null;
+    }
+  },
+  rgb_to_string: function(rgb) {
+    return rgb.r + ", " + rgb.g + ", " + rgb.b;
+  }
+};
+
+module.exports = obj;
+
+
+},{}],2:[function(require,module,exports){
+var H, init_overlay, set_bgcolor, update_color;
+
+H = require('./color_helpers.coffee');
 
 init_overlay = function() {
   var overlay;
@@ -21,7 +55,7 @@ set_bgcolor = function(element, color, opacity) {
   if (color != null) {
     return element.style['background-color'] = "rgba(" + color + ", " + opacity + ")";
   } else {
-    return element.style['background-color'] = 'transparent';
+    return element.style['background-color'] = "transparent";
   }
 };
 
@@ -34,7 +68,7 @@ update_color = function(element) {
       if (!items.custom) {
         return set_bgcolor(element, items.rgb, items.opacity);
       } else {
-        return set_bgcolor(element, items.custom_color, items.opacity);
+        return set_bgcolor(element, H.rgb_to_string(H.hex_to_rgb(items.custom_color)), items.opacity);
       }
     } else {
       return set_bgcolor(element);
@@ -51,4 +85,4 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 
-},{}]},{},[1])
+},{"./color_helpers.coffee":1}]},{},[2])
